b2e69e5a41db7a8d00a0d6c3a256899f669d79fe
Armin Daryabegi <adaryabegi>
2020-04-02 23:01:12 +0200

    Updates

---
+++ precache-manifest.533da8a1e9bc631569313651fcb4e3d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745d66cf8bd2b3841a9e97e283c42132",
+    "url": "/login-simplyplanned/index.html"
+  },
+  {
+    "revision": "ee7d68a63da4ec83a61a",
+    "url": "/login-simplyplanned/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "486be9d51ec9b3de17b3",
+    "url": "/login-simplyplanned/static/css/main.b5707c2d.chunk.css"
+  },
+  {
+    "revision": "ee7d68a63da4ec83a61a",
+    "url": "/login-simplyplanned/static/js/2.c8e5ad52.chunk.js"
+  },
+  {
+    "revision": "d05926d49d122e0d92a31e315ba86c0c",
+    "url": "/login-simplyplanned/static/js/2.c8e5ad52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486be9d51ec9b3de17b3",
+    "url": "/login-simplyplanned/static/js/main.633497ba.chunk.js"
+  },
+  {
+    "revision": "fcb131836c3bbcebf8fe",
+    "url": "/login-simplyplanned/static/js/runtime-main.a9254bee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/login-simplyplanned/static/media/logo.5d5d9eef.svg"
+  }
+]);
