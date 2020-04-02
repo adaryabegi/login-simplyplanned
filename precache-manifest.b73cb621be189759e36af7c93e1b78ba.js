9ff3ef08b36b4cdcf447a69bf25c51bc3b9f6a98
Armin Daryabegi <adaryabegi>
2020-04-02 23:17:38 +0200

    Updates

---
+++ precache-manifest.b73cb621be189759e36af7c93e1b78ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1090b286579408b976408beb031271d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f15eb3e0138223f16587",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "47675f7e29776815cf09",
+    "url": "/static/css/main.780d01b8.chunk.css"
+  },
+  {
+    "revision": "f15eb3e0138223f16587",
+    "url": "/static/js/2.7838371f.chunk.js"
+  },
+  {
+    "revision": "d05926d49d122e0d92a31e315ba86c0c",
+    "url": "/static/js/2.7838371f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47675f7e29776815cf09",
+    "url": "/static/js/main.90a204e9.chunk.js"
+  },
+  {
+    "revision": "b89996781072ee6326be",
+    "url": "/static/js/runtime-main.e5823f5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
